e1a0563714acfc4478338096e0a0cbf3a375a415
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-09-04 16:54:39 -0700

    Add: basic chunk machinary

--- src/chunk.c
+++ src/chunk.c
@@ -9,14 +9,30 @@ void initChunk(Chunk* chunk) {
   chunk->code = NULL;
 }
 
+void freeChunk(Chunk *chunk) {
+  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  initChunk(chunk);
+}
+
 
 void writeChunk(Chunk* chunk, uint8_t byte) {
   if (chunk->capacity < chunk->count + 1) {
     int oldCapacity = chunk->capacity;
     chunk->capacity = GROW_CAPACITY(oldCapacity);
-    chunk->code = GROW_ARRAY(unit8_t, chunk->code, oldCapacity, chunk->capacity);
+    chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
   }
 
   chunk->code[chunk->count] = byte;
   chunk->count++;
+}
+
+void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
+  if (newSize == 0) {
+    free(pointer);
+    return NULL;
+  }
+
+  void* result = realloc(pointer, newSize);
+  if (result == NULL) exit(1);
+  return result;
 }

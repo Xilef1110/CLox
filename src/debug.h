9fa44d80340cb384a9b050ef6e073d771dab3144
Felix <74522795+Xilef1110@users.noreply.github.com>
2026-09-05 12:00:40 -0700

    Add: Constant Representation

--- src/debug.h
+++ src/debug.h
@@ -4,6 +4,8 @@
 #include "chunk.h"
 
 void disassembleChunk(Chunk* chunk, const char* name);
+
+// Returns: Offset to beggining of next instruction
 int disassembleInstruction(Chunk* chunk, int offset);
 
 
